cda98608ea920f24f02ba7470e8a717fbceb2115
RobertFuel <r.garciax2@gmail.com>
2023-10-04 18:29:55 -0300

    Update: App Tareas

--- tareas/views.py
+++ tareas/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import login
+from django.db import IntegrityError
+
 
 def home(request):
     return render(request, 'tareas/home.html')
@@ -12,10 +14,25 @@ def signup(request):
         return render(request, 'tareas/signup.html', {'form': UserCreationForm})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(
+            try:
+                user = User.objects.create_user(
                 username=request.POST['username'],
-                password=request.POST['password1'],
+                password=request.POST['password1'])
                 user.save()
-            )
-    
+                login(request, user)
+                return redirect('tareas')
+            except IntegrityError:
+                return render(request, 'tareas/signup.html', {
+                    'form': UserCreationForm,
+                    "error": "El usuario ya existe"
+                })
+        return render(request, 'tareas/signup.html', {
+                    'form': UserCreationForm,
+                    "error": "Contraseñas no coinciden"
+                })
+            
+        
     return render(request, 'tareas/signup.html', {'form': UserCreationForm})
+
+def tareas(request):
+    return render(request, 'tareas/tareas.html')
